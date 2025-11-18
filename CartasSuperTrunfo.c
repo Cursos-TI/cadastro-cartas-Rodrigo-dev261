54576329d14371fb1277ae0c1c198b4c9f1d06c1
Rodrigo F. Silva <103850462+Rodrigo-dev261@users.noreply.github.com>
2025-11-18 02:24:12 +0000

    registrei segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,14 +8,65 @@ int main() {
   
   // Área para definição das variáveis para armazenar as propriedades das cidades
   
-  int carta, p_turisticos;
-  char estado;
-  char codigo[3];
+  int carta1, p_turisticos1, populacao1;
+  char estado1[20], codigo1[3], n_cidade1[20];
+  float area_em_km1, pib1;
 
+  // Dados da carta 2
+
+  int carta2, p_turisticos2, populacao2;
+  char estado2[20], codigo2[3], n_cidade2[20];
+  float area_em_km2, pib2;
+  
   // Área para entrada de dados
 
+  printf("Bem vindo ao jogo Super Trunfo Países!\n");
 
+  printf("Digite o número da carta:");
+  scanf("%d", &carta1);
 
+  printf("Digite a letra referênte ao Estado de A a H:");
+  scanf("%s", estado1);
+
+  printf("Digite o Código da carta:");
+  scanf("%s", codigo1);
+
+  printf("Digite o nome da Cidade:");
+  scanf("%s", n_cidade1);
+
+  printf("Digite o número da populacional:");
+  scanf("%d", &populacao1);
+
+  printf("Digite a área em Km²:");
+  scanf("%f", &area_em_km1);
+
+  printf("Digite i PIB da Cidade:");
+  scanf("%f", &pib1);
+
+  printf("Digite o número de pontos turisticos:");
+  scanf("%d", &p_turisticos1);
+
+  // Entrada de dados da carta 2.
+
+  printf("Digite os dados da segunda carta.\n");
+  printf("Digite o número da carta:");
+  scanf("%d", &carta2);
+
+  printf("Digite a letra referênte ao Estado de A a H:");
+  scanf("%s", estado2);
+
+  printf("Digite o código da carta:");
+  scanf("%s", codigo2);
+
+  printf("Digite o nome da Cidade:");
+  scanf("%s", n_cidade2);
+
+  printf("Digite o número populacional:");
+  scanf("%f", &populacao2);
+
+  printf("Digite a área em Km");
+  
+  
   // Área para exibição dos dados da cidade
 
 return 0;
